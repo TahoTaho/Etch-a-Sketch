d3a82b052b158ea9f3e367a550fc1e9d55530c06
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-07-15 19:27:16 +0800

    Table done and Mousehover effect to be continued

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('div.container');
+
+function makeGrid(columns, rows) {
+    for (let i = 1; i <= columns; i++) {
+    const column = document.createElement('div');
+    column.className = 'column';
+        for (let j = 1; j <= rows; j++) {
+        const row = document.createElement('div');
+            row.className = 'row';
+            column.append(row);
+            mouseOver(row);
+        }
+        container.append(column);
+    }
+}
+
+function mouseOver(element) {
+    element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'red';
+    });
+}
+
+makeGrid(33,15);
+
+
+
