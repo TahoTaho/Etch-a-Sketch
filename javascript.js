d51c061d88fdbb6b1faedd37a74aa66b491df8b0
Kyle Salvador <kizutsuitemotachiagaru@gmail.com>
2023-07-16 09:14:45 +0800

    Skectpad button fully functional

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
+
 function makeGrid(columns, rows) {
-    const container = document.querySelector('div.container');
     for (let i = 1; i <= columns; i++) {
     const column = document.createElement('div');
     column.className = 'column';
@@ -7,20 +7,42 @@ function makeGrid(columns, rows) {
         const row = document.createElement('div');
             row.className = 'row';
             column.append(row);
-            mouseOver(row);
+            row.addEventListener('mouseover', changeColor);
         }
         container.append(column);
     }
 }
 
-function mouseOver(element) {
-    element.addEventListener('mouseover', () => {
-        element.setAttribute('style', 'background-color:red;');
-    });
+function changeColor() {
+    this.setAttribute('style', 'background-color:red;');
 }
 
-makeGrid(100,100);
+function newGrid() {
+    let inputHeight = prompt('Enter grid height (Maxium is 100)', '');
+    let inputWidth = prompt('Enter width (Maxium is 100)', '');
 
+    const height = parseInt(inputHeight);
+    const width = parseInt(inputWidth);
+
+    if (height > 100 || width > 100) {
+        alert('Maximum you can input is 100, Try again');
+    }
+    else {
+        removeContents(container);
+        makeGrid(height,width);
+    }   
+}
+
+function removeContents(element) {
+    while(element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+const container = document.querySelector('div.container');
+const button = document.querySelector('button');
+button.addEventListener('click', newGrid);
+makeGrid(60,60);
 
 
 
